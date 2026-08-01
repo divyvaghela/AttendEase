f4809d3195a23cdcb54d5ca9dc97a178ee161347
Divy Vaghela <divyvaghela63@gmail.com>
2026-08-01 12:03:25 +0530

    connected mongo db atlas

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +13,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+connectDB();
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
